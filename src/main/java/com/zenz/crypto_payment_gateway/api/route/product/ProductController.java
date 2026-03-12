248aed8b9e685e3f390ec455a6d60a1aa8ab8d97
LucioJamieAI <148231849+JadoreThompson@users.noreply.github.com>
2026-03-12 23:23:08 +0000

    wip: Created CRUD APIs (product api) - Implemented ProductController

--- src/main/java/com/zenz/crypto_payment_gateway/api/route/product/ProductController.java
+++ src/main/java/com/zenz/crypto_payment_gateway/api/route/product/ProductController.java
@@ -3,27 +3,77 @@ package com.zenz.crypto_payment_gateway.api.route.product;
 import com.zenz.crypto_payment_gateway.api.route.product.request.CreateProductRequest;
 import com.zenz.crypto_payment_gateway.api.route.product.request.UpdateProductRequest;
 import com.zenz.crypto_payment_gateway.api.route.product.response.ProductResponse;
+import com.zenz.crypto_payment_gateway.entity.Merchant;
+import com.zenz.crypto_payment_gateway.entity.Product;
+import com.zenz.crypto_payment_gateway.entity.User;
+import com.zenz.crypto_payment_gateway.service.MerchantService;
+import com.zenz.crypto_payment_gateway.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
-@RequestMapping("/product/")
+@RequestMapping("/merchants/{merchantId}/product/")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService  productService;
+    private final MerchantService merchantService;
+
     @PostMapping("/")
-    public void createProduct(@RequestBody CreateProductRequest body) {}
+    public ResponseEntity<?> createProduct(
+            @AuthenticationPrincipal User user,
+            @PathVariable UUID merchantId,
+            @RequestBody CreateProductRequest body
+    ) {
+        Merchant merchant = merchantService.getMerchantByIdAndUserId(merchantId, user.getUserId());
+        Product product = new Product();
 
-    @GetMapping("/{productId}/")
-    public ProductResponse getProduct(@PathVariable String productId) {return null;}
+        product.setMerchantId(merchantId);
+        product.setMerchant(merchant);
+        product.setName(body.getName());
+        product.setDescription(body.getDescription());
 
-    @GetMapping("/")
-    public List<ProductResponse> getProducts() {return null;}
+        product = productService.createProduct(product);
 
-    @PutMapping("/{productId}/")
-    public ProductResponse updateProduct(@RequestBody UpdateProductRequest body, @PathVariable String productId) {
-        return null;
+        return ResponseEntity.ok(productService.toResponse(product));
     }
 
-    @DeleteMapping("/{productId}/")
-    public void deleteProduct(@PathVariable String productId) {}
+    @GetMapping("/{productId}/")
+    public ResponseEntity<?> getProduct(
+            @AuthenticationPrincipal User user,
+            @PathVariable UUID merchantId,
+            @PathVariable UUID productId
+    ) {
+        merchantService.getMerchantByIdAndUserId(merchantId, user.getUserId());
+        Product product = productService.getProductsByIdAndMerchantId(productId, merchantId);
+        return ResponseEntity.ok(productService.toResponse(product));
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<?> getProducts(
+            @AuthenticationPrincipal User user,
+            @PathVariable UUID merchantId
+    ) {
+        merchantService.getMerchantByIdAndUserId(merchantId, user.getUserId());
+        List<Product> products = productService.getProductsByMerchantId(merchantId);
+        return ResponseEntity.ok(products);
+    }
+
+    @PutMapping("/{productId}/")
+    public ResponseEntity<Product> updateProduct(
+            @AuthenticationPrincipal User user,
+            @PathVariable UUID merchantId,
+            @PathVariable UUID productId,
+            @RequestBody UpdateProductRequest body
+    ) {
+        merchantService.getMerchantByIdAndUserId(merchantId, user.getUserId());
+        Product product = productService.getProductsByIdAndMerchantId(productId, merchantId);
+        product = productService.updateProduct(product, body);
+        return ResponseEntity.ok(product);
+    }
 }
