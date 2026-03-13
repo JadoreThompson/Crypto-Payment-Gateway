b10e37d0f941b2f95dec229af1603d13a8491206
LucioJamieAI <148231849+JadoreThompson@users.noreply.github.com>
2026-03-13 22:55:05 +0000

    wip: Created API tests (auth api) - Implemented NotBlank validation for LoginRequest fields

--- src/main/java/com/zenz/crypto_payment_gateway/api/route/auth/model/request/LoginRequest.java
+++ src/main/java/com/zenz/crypto_payment_gateway/api/route/auth/model/request/LoginRequest.java
@@ -1,9 +1,13 @@
 package com.zenz.crypto_payment_gateway.api.route.auth.model.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class LoginRequest {
+    @NotBlank
     private String email;
+
+    @NotBlank
     private String password;
 }
