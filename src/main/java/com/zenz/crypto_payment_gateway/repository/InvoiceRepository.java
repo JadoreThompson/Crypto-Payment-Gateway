475469d566d5cf6bdc98533f1b90cd1007b7813b
LucioJamieAI <148231849+JadoreThompson@users.noreply.github.com>
2026-03-13 10:16:15 +0000

    wip: Created CRUD APIs (invoice api) - Created InvoiceRepository

---
+++ src/main/java/com/zenz/crypto_payment_gateway/repository/InvoiceRepository.java
@@ -0,0 +1,18 @@
+package com.zenz.crypto_payment_gateway.repository;
+
+import com.zenz.crypto_payment_gateway.entity.Invoice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface InvoiceRepository extends JpaRepository<Invoice, UUID> {
+
+    List<Invoice> findByCustomerId(String customerId);
+
+    List<Invoice> findByMerchantId(UUID merchantId);
+
+    Invoice findByIdAndMerchantId(UUID id, UUID merchantId);
+}
