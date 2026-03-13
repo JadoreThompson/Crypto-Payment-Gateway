df5995435ba79203f5ce292107850c59877c0d6f
LucioJamieAI <148231849+JadoreThompson@users.noreply.github.com>
2026-03-13 09:39:41 +0000

    wip: Created CRUD APIs (price api) - Created PriceRepository

---
+++ src/main/java/com/zenz/crypto_payment_gateway/repository/PriceRepository.java
@@ -0,0 +1,20 @@
+package com.zenz.crypto_payment_gateway.repository;
+
+import com.zenz.crypto_payment_gateway.entity.Price;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface PriceRepository extends JpaRepository<Price, UUID> {
+
+    public List<Price> findByMerchantId(UUID merchantId);
+
+    public List<Price> findByMerchantIdAndProductId(UUID merchantId, UUID productId);
+
+    public List<Price> findByProductId(UUID productId);
+
+    public Price findByIdAndProductId(UUID id, UUID productId);
+}
