b5498feb2db638ab39ff09cd0661d78136033803
LucioJamieAI <148231849+JadoreThompson@users.noreply.github.com>
2026-03-13 13:05:16 +0000

    wip: Created API tests (auth api) - Added field validation to RegisterRequest

--- src/main/java/com/zenz/crypto_payment_gateway/api/route/auth/model/request/RegisterRequest.java
+++ src/main/java/com/zenz/crypto_payment_gateway/api/route/auth/model/request/RegisterRequest.java
@@ -1,9 +1,36 @@
 package com.zenz.crypto_payment_gateway.api.route.auth.model.request;
 
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class RegisterRequest {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email")
     private String email;
+
+    @NotBlank(message = "Password is required")
     private String password;
+
+    @AssertTrue(message = "Password length must be greater than or equal to 8 characters")
+    private boolean checkPasswordLength() {
+        return password.length() >= 8;
+    }
+
+    @AssertTrue(message = "Password must have at least 2 upper case characters")
+    private boolean checkPasswordUpperCase() {
+        final int minUpperCase = 2;
+        int count = 0;
+
+        for (char c : password.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                count++;
+                if (count >= minUpperCase) break;
+            }
+        }
+
+        return count >= minUpperCase;
+    }
 }
