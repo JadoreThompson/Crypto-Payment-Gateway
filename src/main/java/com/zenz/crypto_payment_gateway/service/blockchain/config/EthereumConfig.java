8c3055ceebb694da2da9c72ae095e3acb0b53d2d
LucioJamieAI <148231849+JadoreThompson@users.noreply.github.com>
2026-03-15 21:30:33 +0000

    wip: Implemented BlockchainWatcher (Ethereum) - Added logging to EthereumConfig

--- src/main/java/com/zenz/crypto_payment_gateway/service/blockchain/config/EthereumConfig.java
+++ src/main/java/com/zenz/crypto_payment_gateway/service/blockchain/config/EthereumConfig.java
@@ -1,5 +1,6 @@
 package com.zenz.crypto_payment_gateway.service.blockchain.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,12 +10,15 @@ import org.web3j.protocol.websocket.WebSocketService;
 import java.net.ConnectException;
 
 @Configuration
+@Slf4j
 public class EthereumConfig {
 
     @Bean(destroyMethod = "shutdown")
     public Web3j web3j(@Value("${ethereum.ws-url}") String wsUrl) throws ConnectException {
+        log.info("Connecting to Web3j");
         WebSocketService service = new WebSocketService(wsUrl, true);
         service.connect();
+        log.info("Web3j connected successfully");
         return Web3j.build(service);
     }
 }
