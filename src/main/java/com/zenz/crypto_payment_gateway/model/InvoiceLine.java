b5eaf5dce559625ccf499e5e67bd1095a4dfc2c3
LucioJamieAI <148231849+JadoreThompson@users.noreply.github.com>
2026-03-15 21:30:56 +0000

    wip: Implemented BlockchainWatcher (Ethereum) - Added min amount validation to InvoiceLine

--- src/main/java/com/zenz/crypto_payment_gateway/model/InvoiceLine.java
+++ src/main/java/com/zenz/crypto_payment_gateway/model/InvoiceLine.java
@@ -1,11 +1,13 @@
 package com.zenz.crypto_payment_gateway.model;
 
 import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.Min;
 import lombok.Data;
 
 @Data
 @Embeddable
 public class InvoiceLine {
+    @Min(1)
     private long amount;
 
     private String description;
